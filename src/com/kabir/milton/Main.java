9db9deeccbccc76db56e2a0fd21769245f887e34
milton-kabir <milton.cse.pust@gmail.com>
2021-07-31 10:29:26 +0600

    Second Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,13 +1,21 @@
 //package flashcards;
 package com.kabir.milton;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Card:\n" +
-                "purchase\n" +
-                "Definition:\n" +
-                "buy");
+        Scanner sc=new Scanner(System.in);
+        String tr=sc.nextLine();
+        String def=sc.nextLine();
+        String ans=sc.nextLine();
+        if(def.equals(ans)){
+            System.out.println("Your answer is right!");
+        }
+        else{
+            System.out.println("Your answer is wrong...");
+        }
     }
 }
