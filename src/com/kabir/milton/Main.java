bbac4ad81c45ea4c928cf3c8685269eecf8e5c60
milton-kabir <milton.cse.pust@gmail.com>
2021-07-31 10:58:16 +0600

    Third Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,21 +1,40 @@
 //package flashcards;
 package com.kabir.milton;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Scanner sc=new Scanner(System.in);
-        String tr=sc.nextLine();
-        String def=sc.nextLine();
-        String ans=sc.nextLine();
-        if(def.equals(ans)){
-            System.out.println("Your answer is right!");
+        System.out.println("Input the number of cards:");
+        int tc=Integer.parseInt(sc.nextLine());
+        HashMap<String, String> myMap = new HashMap<>();
+        List<String > ar=new ArrayList<>();
+        for(int i=1;i<=tc;i++){
+            System.out.println("Card #"+i+":");
+            String st= sc.nextLine();
+            System.out.println("The definition for card #"+i+":");
+            String ts=sc.nextLine();
+            myMap.put(st,ts);
+            ar.add(st);
         }
-        else{
-            System.out.println("Your answer is wrong...");
+        for(int i=0;i<ar.size();i++){
+            System.out.println("Print the definition of \""+ar.get(i)+"\":");
+            String st= sc.nextLine();
+            String ts=myMap.get(ar.get(i));
+            if(st.equals(ts)){
+                System.out.println("Correct!");
+            }
+            else{
+                System.out.println("Wrong. The right answer is \""+ts+"\".");
+            }
         }
+
     }
 }
