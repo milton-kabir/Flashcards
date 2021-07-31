aa005119fd4b2ddec6c3b7a08d512f856fa8c581
milton-kabir <milton.cse.pust@gmail.com>
2021-07-31 10:16:57 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,13 @@
+//package flashcards;
 package com.kabir.milton;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Card:\n" +
+                "purchase\n" +
+                "Definition:\n" +
+                "buy");
     }
 }
